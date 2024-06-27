b1e262694a0d1966e0b8c929d4c46ea831f0da4f
doorkaz <retamalespj@gmail.com>
2024-06-27 18:05:33 -0400

    Protege las variables privadas de la base de datos en un .env

--- server/Database/index.js
+++ server/Database/index.js
@@ -1,14 +1,18 @@
 const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 
+dotenv.config();
 
-const MONGO_URL = "mongodb+srv://juancucurella:oqHxUHKlE2eyPBMG@bd-proyectoadsw.ppsmpm5.mongodb.net/?retryWrites=true&w=majority";
+const { DB_USER, DB_PASSWORD, DB_HOST } = process.env
+
+const MONGO_URL = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/?retryWrites=true&w=majority`;
 
 const db = async () => {
     try {
         const conn = await mongoose.connect(MONGO_URL);
-        console.log("BD CONNECTED", conn.connection.host);
+        console.log("Se ha conectado a la base de datos", conn.connection.host);
     } catch (error){
-        console.log(error);
+        console.error(error);
     }
 }
 
